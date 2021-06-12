5874396a9c8bcb3b368abe938ac3117a09293bb8
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-12 16:05:15 -0400

    User interface for home, login and sign up page

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import HomePage from './containers/HomePage';
+import LoginPage from './containers/LoginPage';
+import RegisterPage from './containers/RegisterPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path= '/' exact component= {HomePage} />
+        <Route path= '/login' component= {LoginPage} />
+        <Route path= '/signup' component= {RegisterPage} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
