b24c707a6c84d7ac5a4f9877a46fa138abbab488
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-15 16:09:58 -0400

    User chat interface, login and signup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
-export default () => {
-    return { user : 'dan'}
-};
+import { combineReducers } from 'redux';
+import authReducer  from './auth.reducer';
+
+const rootReducer = combineReducers ({
+    auth: authReducer
+});
+
+export default rootReducer;
