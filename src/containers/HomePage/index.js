b0b8ba0621a6dd248d403dd8d641495cef0c7300
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-15 16:09:13 -0400

    User chat interface, login and signup

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
-import Layout from '../../components/Layout';
+import './style.css';
 
 const HomePage = (props) => {
-    return (
-        <Layout>
-            <div>HomePage</div>
-        </Layout>
-    )
+  return (
+    <section className="container">
+    <div className="listOfUsers">
+
+        <div className="displayName">
+            <div className="displayPic">
+                {/* Change this image */}
+                <img src="https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg" alt="" />
+            </div>
+            <div style={{margin: '0 10px'}}>
+                <span style={{fontWeight: 500}}>Dan Um</span>
+            </div>
+        </div>
+                
+    </div>
+
+    <div className="chatArea">
+        <div className="chatHeader"> Dan Um </div>
+        <div className="messageSections">
+
+            <div style={{ textAlign: 'left' }}>
+                <p className="messageStyle" >Hello User</p>
+            </div>
+
+        </div>
+        <div className="chatControls">
+            <textarea />
+            <button>Send</button>
+        </div>
+    </div>
+</section>
+  );
 }
 
 export default HomePage;
