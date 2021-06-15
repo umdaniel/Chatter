743657e380d6c3688fecffca2d426ccc70cabc11
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-15 16:10:25 -0400

    User chat interface, login and signup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './containers/HomePage';
 import LoginPage from './containers/LoginPage';
 import RegisterPage from './containers/RegisterPage';
+import PrivateRoute from './components/PrivateRoute';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { isLoggedInUser } from './actions';
 
 function App() {
+
+  const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!auth.authenticacted) {
+        dispatch(isLoggedInUser());
+    }
+  }, []);
+
   return (
     <div className="App">
       <Router>
-        <Route path= '/' exact component= {HomePage} />
+        {/*Logged in user can access home route*/}
+        <PrivateRoute path= '/' exact component= {HomePage} />
         <Route path= '/login' component= {LoginPage} />
         <Route path= '/signup' component= {RegisterPage} />
       </Router>
